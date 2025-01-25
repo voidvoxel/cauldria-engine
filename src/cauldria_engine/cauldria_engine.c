1d5479b6c9dc1aece310077854baf27e91c221bf
Ashlynn Juniper <160202125+voidvoxel@users.noreply.github.com>
2025-01-25 17:59:18 -0600

    Create cauldria_engine.c

---
+++ src/cauldria_engine/cauldria_engine.c
@@ -0,0 +1,27 @@
+#include <cauldria_engine.h>
+
+char *DEFAULT_APPLICATION_PATH = "debug";
+
+int main(int argc, char **argv) {
+    char *application_path = DEFAULT_APPLICATION_PATH;
+
+    // Start the application engine.
+    struct cauldria_engine *cauldria_engine = cauldria_start_engine();
+
+    // Load the application.
+    struct cauldria_engine_application *application
+        = cauldria_load_application(
+            cauldria_engine,
+            application_path
+        );
+
+    // Start the application.
+    int error_code
+        = cauldria_start_application(
+            cauldria_engine,
+            application
+        );
+
+    // Return the exit code returned by the application.
+    return error_code;
+}
