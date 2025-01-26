01ee9cc562a960921ad6302c06c4efc2ceb8c675
Ashlynn Juniper <160202125+voidvoxel@users.noreply.github.com>
2025-01-25 20:57:41 -0600

    Wrap application code prior to execution

--- src/cauldria_engine/libcauldria_engine.c
+++ src/cauldria_engine/libcauldria_engine.c
@@ -1,8 +1,22 @@
+#include <string.h>
+
 #include <cauldria_engine.h>
 #include <path.h>
 #include <raylib.h>
 
 /* Private API */
+
+char* _cauldria_concat(const char *s1, const char *s2)
+{
+    const size_t len1 = strlen(s1);
+    const size_t len2 = strlen(s2);
+    char *result = malloc(len1 + len2 + 1); // +1 for the null-terminator
+    // in real code you would check for errors in malloc here
+    memcpy(result, s1, len1);
+    memcpy(result + len1, s2, len2 + 1); // +1 to copy the null-terminator
+    return result;
+}
+
 
 void _cauldria_initialize_script_engine (
     struct cauldria_engine_application *application
@@ -11,6 +25,10 @@ void _cauldria_initialize_script_engine (
 void _cauldria_load_application_source_code (
     struct cauldria_engine_application *application,
     char *application_path
+);
+
+char * _cauldria_wrap_application_code (
+    char *source_code_body
 );
 
 void _cauldria_initialize_script_engine (
@@ -24,6 +42,52 @@ void _cauldria_initialize_script_engine (
     luaL_openlibs(L);
 }
 
+void _cauldria_run_application (
+    struct cauldria_engine_application *application
+)
+{
+    // Get the application instance's Lua state.
+    lua_State *L = application->L;
+
+    // Get the application's source code.
+    char *source_code_header = ""
+        "-- Define the application default values.\n"
+        "application = {\n"
+        "   name = \"application\",\n"
+        "   version = \"0.0.0\",\n"
+        "   dependencies = {}\n"
+        "}\n\n"
+    ;
+
+    char *source_code_footer = ""
+        "-- Emit the exit event.\n"
+        "if type(application.on_exit) == \"function\" then\n"
+        "    application:on_exit()\n"
+        "end\n"
+        "print (application)\n"
+    ;
+
+    char *source_code = _cauldria_wrap_application_code(application->source_code);
+
+    // Run the source code header.
+    if (luaL_dostring(L, source_code_header) == LUA_OK) {
+        // Remove the loaded function from the stack.
+        lua_pop(L, lua_gettop(L));
+    }
+
+    // Run the application's source code.
+    if (luaL_dostring(L, source_code) == LUA_OK) {
+        // Remove the loaded function from the stack.
+        lua_pop(L, lua_gettop(L));
+    }
+
+    // Run the source code footer.
+    if (luaL_dostring(L, source_code_footer) == LUA_OK) {
+        // Remove the loaded function from the stack.
+        lua_pop(L, lua_gettop(L));
+    }
+}
+
 void _cauldria_load_application_source_code (
     struct cauldria_engine_application *application,
     char *application_path
@@ -34,6 +98,28 @@ void _cauldria_load_application_source_code (
 
     // Load the application's source code from the `init.lua` file.
     application->source_code = LoadFileText(init_script_path);
+}
+
+char * _cauldria_wrap_application_code (
+    char *source_code_body
+)
+{
+    char *source_code_prefix = ""
+        "-- Run the application's source code.\n"
+        "(function ()\n"
+    ;
+
+    char *source_code_suffix = ""
+        "\n"
+        "end)()\n"
+    ;
+
+    char *_source_code_tmp = _cauldria_concat(source_code_prefix, source_code_body);
+    char *source_code = _cauldria_concat(_source_code_tmp, source_code_suffix);
+
+    free(_source_code_tmp);
+
+    return source_code;
 }
 
 /* Public API */
@@ -70,6 +156,9 @@ struct cauldria_engine_application * cauldria_load_application (
     // Initialize the script engine.
     _cauldria_initialize_script_engine(application);
 
+    // Run the application's source code.
+    _cauldria_run_application(application);
+
     // Return the application instance.
     return application;
 }
@@ -78,18 +167,6 @@ int cauldria_start_application (
     struct cauldria_engine_application *application
 )
 {
-    // Get the application instance's Lua state.
-    lua_State *L = application->L;
-
-    // Get the application's source code.
-    char *source_code = application->source_code;
-
-    // Run the application's source code.
-    if (luaL_dostring(L, source_code) == LUA_OK) {
-        // Remove the loaded function from the stack.
-        lua_pop(L, lua_gettop(L));
-    }
-
     // Note that the application is running.
     application->is_running = true;
 
