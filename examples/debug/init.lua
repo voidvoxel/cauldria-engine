581bad3bd4735d6a01b178db8c9ed71bbbbdeeb7
Ashlynn Juniper <160202125+voidvoxel@users.noreply.github.com>
2025-01-26 03:37:31 -0600

    Move debug application to examples

--- examples/debug/init.lua
+++ examples/debug/init.lua
@@ -2,6 +2,18 @@ application.name = "debug"
 application.version = "0.0.0"
 application.dependencies = {}
 
+function application:awake ()
+
+end
+
+function application:draw ()
+
+end
+
 function application:on_exit ()
     print "Exiting..."
+end
+
+function application:tick ()
+
 end
