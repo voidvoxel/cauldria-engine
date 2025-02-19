e9f4fb579c0d755d627f5830bba9dd64ad9baa88
ashlynn-juniper <160202125+voidvoxel@users.noreply.github.com>
2025-02-19 04:37:49 -0600

    Create cauldria.cpp

---
+++ src/cauldria.cpp
@@ -0,0 +1,9 @@
+#include "voidvoxel/cauldria/engine/engine.cpp"
+
+
+int main(int argc, char const *argv[])
+{
+    voidvoxel::cauldria::Window *window = new voidvoxel::cauldria::Window();
+
+    return 0;
+}
