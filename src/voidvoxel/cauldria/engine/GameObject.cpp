7e99cf1b41bfe91eb27589c6290d99d2cba52927
ashlynn-juniper <160202125+voidvoxel@users.noreply.github.com>
2025-02-19 04:38:31 -0600

    Create GameObject.cpp

---
+++ src/voidvoxel/cauldria/engine/GameObject.cpp
@@ -0,0 +1,26 @@
+#if !defined(VOIDVOXEL__CAULDRIA__GAME_OBJECT_CPP)
+#define VOIDVOXEL__CAULDRIA__GAME_OBJECT_CPP
+
+#include "GameObject.hpp"
+
+voidvoxel::cauldria::GameObject::GameObject()
+{
+
+}
+
+voidvoxel::cauldria::GameObject::~GameObject()
+{
+
+}
+
+void voidvoxel::cauldria::GameObject::render()
+{
+
+}
+
+void voidvoxel::cauldria::GameObject::tick()
+{
+
+}
+
+#endif // VOIDVOXEL__CAULDRIA__GAME_OBJECT_CPP
